63748710e20b6c9dad9e8fe8acbcb57a3a995b05
anamitsutani <ana.mitsutani@gmail.com>
2023-08-23 02:18:21 +0900

    chore: add jest config and scripts

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,12 @@ const config = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/**.ts'],
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  testEnvironment: "node",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
   transform: {
-    ".+\\.ts$": "ts-jest"
+    '.+\\.ts$': 'ts-jest'
   }
-};
+}
 
-module.exports = config;
+module.exports = config

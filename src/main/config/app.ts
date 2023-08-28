648e438b1dbe23e57742783c10307edfcd9f61b1
anamitsutani <ana.mitsutani@gmail.com>
2023-08-29 01:46:00 +0900

    feat: ensure signup route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import setUpMiddlewares from '../config/middlewares'
+import setUpRoutes from '../config/routes'
 
 const app = express()
 setUpMiddlewares(app)
+setUpRoutes(app)
 export default app

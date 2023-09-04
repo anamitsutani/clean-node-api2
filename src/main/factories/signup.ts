f6ffb09ba9b1da57a3d38b2662d27254034c428e
anamitsutani <ana.mitsutani@gmail.com>
2023-09-05 06:30:59 +0900

    refactor: move email validation to EmailValidationComposite

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,5 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { LogControllerDecorator } from '../decorators/log'
@@ -9,12 +8,11 @@ import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
-  const emailValidatorAdapter = new EmailValidatorAdapter()
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount, makeSignUpValidation())
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }

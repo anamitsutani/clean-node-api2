a7c5efe28690911e63ea899eb8ea781b273604c6
anamitsutani <ana.mitsutani@gmail.com>
2023-09-07 03:15:28 +0900

    feat: ensure AccountMongoRepository returns an account on email success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -16,5 +16,12 @@ export const MongoHelper = {
       await this.connect(this.uri)
     }
     return this.client.db().collection(name)
+  },
+  map: (data: any): any => {
+    const { _id, ...rest } = data
+    return { ...rest, id: _id.toHexString() }
+  },
+  mapCollection: (collection: any[]): any[] => {
+    return collection.map(c => MongoHelper.map(c))
   }
 }
